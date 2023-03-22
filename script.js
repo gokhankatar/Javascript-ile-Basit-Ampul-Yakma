ba773943c04f1e94b39af397a89bdd39aea15410
Gokhan Katar <katar_gokhan@hotmail.com>
2023-03-22 21:26:09 +0300

    committed all

---
+++ script.js
@@ -0,0 +1,11 @@
+function on() {
+
+    document.getElementById("light").src = "/images/bullbon.jpg";
+
+}
+
+function off() {
+
+    document.getElementById("light").src = "/images/bullboff.jpg";
+
+}
